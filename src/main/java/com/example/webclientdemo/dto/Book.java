1d24a3d854c6a74973bbac432d6bbfd2577efcb0
AmanuelChorito <48349483+AmanuelChorito@users.noreply.github.com>
2022-11-21 15:07:32 -0600

    pr:update using map

--- src/main/java/com/example/webclientdemo/dto/Book.java
+++ src/main/java/com/example/webclientdemo/dto/Book.java
@@ -1,16 +1,16 @@
 package com.example.webclientdemo.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.Map;
 
 @Data
+@JsonInclude(JsonInclude.Include. NON_NULL)
 public class Book {
     @JsonProperty("ISBN:0201558025")
-    //private ISBN0201558025 isbn0201558025;
     private Map<String,String> isbn0201558025;
     @JsonProperty("LCCN:93005405")
-   // private LCCN93005405 lccn93005405;
     private Map<String,String> lccn93005405;
 }
